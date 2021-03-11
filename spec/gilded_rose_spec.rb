fa81441a283f4e390d4ee4c45f841a51a2559bba
bullhornfixie <dawson.jon@hotmail.co.com>
2021-03-11 18:01:40 +0000

    first unit test on GildedRose #update_quality passed

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,11 +3,16 @@ require_relative '../lib/gilded_rose'
 describe GildedRose do
 
   describe "#update_quality" do
-    it "does not change the name" do
+    it "it does not change name of an item" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+      expect(items[0].name).to eq "foo"
     end
+     
+    it "updates the quality value of an item by -1" do 
+      items = [Item.new(name="Elixir of the Mongoose", sell_in=5, quality=7)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 6
+    end 
   end
-
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -6,15 +6,15 @@ describe Item do
   end 
 
   it 'is instantiated with a name' do 
-    expect(@item.name).to eq('Exclusive Item')
+    expect(@item.name).to eq 'Exclusive Item'
   end
 
   it 'is instantiated with a sell_in value which is days to sell item' do 
-    expect(@item.sell_in).to eq(5)
+    expect(@item.sell_in).to eq 5
   end
 
   it 'is instantiated with a quality value which is how valuable item is' do 
-    expect(@item.quality).to eq(8)
+    expect(@item.quality).to eq 8
   end
 end
 
