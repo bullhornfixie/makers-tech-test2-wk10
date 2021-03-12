863c4ab8475678fe014e2a41a9b87d0c9e823cb2
bullhornfixie <dawson.jon@hotmail.co.com>
2021-03-12 12:39:29 +0000

    legacy code fully refactored and tested

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,56 +1,58 @@
 class GildedRose
+  MAX = 50
+  MIN = 0 
 
   def initialize(items)
     @items = items
   end
 
-  def update_quality()
+  def stock_update()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      update_sell_in(item)
+      update_quality(item)
+    end
+  end 
+
+  def update_sell_in(item)
+    unless item.name.include?('Sulfuras, Hand of Ragnaros')
+      item.sell_in -= 1
     end
   end
-end
+
+  def update_quality(item)
+    case 
+      when item.name == 'Aged Brie' then aged_brie_update(item)
+      when item.name == 'Backstage passes to a TAFKAL80ETC concert' then backstage_passes_update(item)
+      when item.name == 'Sulfuras, Hand of Ragnaros' then item.quality = item.quality 
+    else 
+      standard_item_update(item)
+    end
+  end 
+
+  def aged_brie_update(item)
+    if item.quality < MAX 
+      item.quality += 1 
+    end 
+  end 
+
+  def backstage_passes_update(item)
+    case 
+      when item.sell_in <=0  then item.quality = 0
+      when item.sell_in < 5  then item.quality += 3
+      when item.sell_in < 10 then item.quality += 2
+      when item.sell_in > 10 then item.quality += 1
+      when item.sell_in <= 0 then item.quality -= 23
+    end
+  end 
+
+  def standard_item_update(item)
+    case 
+      when item.quality > MIN && item.sell_in < 0 then item.quality -= 2
+      when item.quality > MIN && item.sell_in > 0 then item.quality -= 1
+    end 
+  end 
+end 
+
+
 
 
